56696ebbdb62adf30ca7cae06c4e1758fc02a789
Chibu <atuchibueze@gmail.com>
2022-11-16 08:21:26 -0500

    first commit

---
+++ sandbox.ts
@@ -0,0 +1,18 @@
+let character = "mario";
+let age = 30;
+let isBlackBelt = false;
+
+// character = 20
+character = "luigi";
+
+// age = 'yoshi'
+age = 40;
+
+// isBlackBelt = 5
+isBlackBelt = true;
+
+const circ = (diameter: number) => {
+  return diameter * Math.PI;
+};
+
+console.log(circ(5));
