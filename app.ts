4aa023b6627e284fa4a51e0ee9c9bb660a3a2028
Chibu <atuchibueze@gmail.com>
2022-11-21 11:43:00 -0500

    commit

--- app.ts
+++ app.ts
@@ -1,10 +1,52 @@
-// const anchor = document.querySelector("a")!;
+import { textSpanEnd } from "typescript";
 
-// console.log(anchor.href);
+// interfaces
+interface IsPerson {
+  name: string;
+  age: number;
+  speak(a: string): void;
+  spend(a: number): number;
+}
+
+const me: IsPerson = {
+  name: "shaun",
+  age: 30,
+  speak(text: string): void {
+    console.log(text);
+  },
+  spend(amount: number): number {
+    console.log("I spent", amount);
+    return amount;
+  },
+};
+
+console.log(me);
+
+// classes
+class Invoice {
+  constructor(
+    readonly client: string,
+    private details: string,
+    public amount: number
+  ) {}
+
+  format() {
+    return `${this.client} owes $${this.amount} for ${this.details}`;
+  }
+}
+
+const invOne = new Invoice("mario", "work on the mario website", 250);
+const invTwo = new Invoice("luigi", "work on the luigi website", 300);
+
+const invoices: Invoice[] = [];
+invoices.push(invOne);
+invoices.push(invTwo);
+
+invoices.forEach((inv) => {
+  console.log(inv.client, inv.amount, inv.format());
+});
 
 const form = document.querySelector(".new-item-form") as HTMLFormElement;
-
-// console.log(form.children);
 
 const type = document.querySelector("#type") as HTMLSelectElement;
 const tofrom = document.querySelector("#tofrom") as HTMLInputElement;
@@ -14,5 +56,5 @@ const amount = document.querySelector("#amount") as HTMLInputElement;
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 
-  console.log(type.value, tofrom.value, details.value, amount.value);
+  console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
 });
